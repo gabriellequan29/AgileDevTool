cf496be1010e1d80dd09c797171f63045b5905c7
gabrielle <qwy0129@gmail.com>
2021-07-09 11:14:51 -0400

    add Routes

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem'
+import CreateProjectBotton from './Project/CreateProjectBotton'
 
 
 class Dashboard extends Component {
@@ -11,9 +12,7 @@ class Dashboard extends Component {
                         <div className="col-md-12">
                             <h1 className="display-4 text-center">Projects</h1>
                             <br />
-                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                                Create a Project
-                            </a>
+                            <CreateProjectBotton />
                             <br />
                             <hr />
                             <ProjectItem />
