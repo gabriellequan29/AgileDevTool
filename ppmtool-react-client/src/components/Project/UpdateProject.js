7066211ae9147eaa36646cf55be32debc387ae5b
gabrielle <qwy0129@gmail.com>
2021-07-16 09:33:03 -0400

    update persistance

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react'
 import PropTypes from "prop-types"
 import { connect } from 'react-redux'
-import { getProject } from '../../actions/projectAction'
+import { getProject, createProject } from '../../actions/projectAction'
 
 class UpdateProject extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+                id: "",
+                projectName: "",
+                projectIdentifier: "",
+                description: "",
+                start_date: "",
+                end_date: ""
+        }
+
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const updateProject = {
+            id: this.state.id,
+            projectName: this.state.projectName,
+            projectIdentifier: this.state.projectIdentifier,
+            description: this.state.description,
+            start_date: this.state.start_date,
+            end_date: this.state.end_date
+        }
+
+        this.props.createProject(updateProject, this.props.history);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {
+            id,
+            projectName,
+            projectIdentifier,
+            description,
+            start_date,
+            end_date
+        } = nextProps.project
+
+        this.setState({
+            id,
+            projectName,
+            projectIdentifier,
+            description,
+            start_date,
+            end_date
+        })
+    }
+
+
+
     componentDidMount() {
         const { id } = this.props.match.params;
         this.props.getProject(id, this.props.history);
@@ -17,14 +75,15 @@ class UpdateProject extends Component {
                         <div class="col-md-8 m-auto">
                             <h5 class="display-4 text-center">Update Project form</h5>
                             <hr />
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div class="form-group">
                                     <input 
                                         type="text" 
                                         class="form-control form-control-lg " 
                                         placeholder="Project Name" 
                                         name="projectName"
-                                        value={this.props.project.projectName}
+                                        value={this.state.projectName}
+                                        onChange={this.onChange}
                                         />
                                 </div>
                                 <div class="form-group">
@@ -33,6 +92,8 @@ class UpdateProject extends Component {
                                         class="form-control form-control-lg" 
                                         placeholder="Unique Project ID"
                                         name="projectIdentifier"
+                                        value={this.state.projectIdentifier}
+                                        onChange={this.onChange}
                                         disabled />
                                 </div>
                                 <div class="form-group">
@@ -40,6 +101,8 @@ class UpdateProject extends Component {
                                         class="form-control form-control-lg" 
                                         placeholder="Project Description"
                                         name="description"
+                                        value={this.state.description}
+                                        onChange={this.onChange}
                                     />  
                                 </div>
                                 <h6>Start Date</h6>
@@ -47,19 +110,25 @@ class UpdateProject extends Component {
                                     <input 
                                         type="date" 
                                         class="form-control form-control-lg" 
-                                        name="start_date" />
+                                        name="start_date"
+                                        value={this.state.start_date}
+                                        onChange={this.onChange} />
                                 </div>
                                 <h6>Estimated End Date</h6>
                                 <div class="form-group">
                                     <input 
                                         type="date" 
                                         class="form-control form-control-lg" 
-                                        name="end_date" />
+                                        name="end_date"
+                                        value={this.state.end_date} 
+                                        onChange={this.onChange}
+                                        />
                                 </div>
         
                                 <input 
                                     type="submit" 
-                                    class="btn btn-primary btn-block mt-4" />
+                                    class="btn btn-primary btn-block mt-4"
+                                     />
                             </form>
                         </div>
                     </div>
@@ -71,6 +140,7 @@ class UpdateProject extends Component {
 
 UpdateProject.propTypes = {
     getProject: PropTypes.func.isRequired,
+    createProject: PropTypes.func.isRequired,
     project: PropTypes.object.isRequired
 }
 
@@ -80,5 +150,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    { getProject }
+    { getProject, createProject }
 )(UpdateProject)
